5b9e784ecf0e1bc5336c8c247da5bdc1da5d3be1
Muharrem ERİN <erin.muharrem@gmail.com>
2025-05-15 14:21:02 +0300

    #slug-registry-fix

--- src/main/kotlin/com/mewebstudio/springboot/jpa/slug/kotlin/SlugRegistry.kt
+++ src/main/kotlin/com/mewebstudio/springboot/jpa/slug/kotlin/SlugRegistry.kt
@@ -1,35 +1,35 @@
 package com.mewebstudio.springboot.jpa.slug.kotlin
 
 /**
- * Registry for managing the global [ISlugProvider].
+ * Registry for managing the global [slugProvider].
  *
- * This class is used to register and retrieve the [ISlugProvider] used for generating slugs.
- * The [ISlugProvider] is intended to be set at the application startup and used throughout the lifecycle
+ * This class is used to register and retrieve the [slugProvider] used for generating slugs.
+ * The [slugProvider] is intended to be set at the application startup and used throughout the lifecycle
  * of slug generation operations.
  */
 object SlugRegistry {
     /**
-     * The global [ISlugProvider] instance.
+     * The global [slugProvider] instance.
      *
      * This instance is used for generating slugs across the application.
      */
-    private var ISlugProvider: ISlugProvider? = null
+    private var slugProvider: ISlugProvider? = null
 
     /**
-     * Sets the [ISlugProvider] to be used globally for slug generation.
+     * Sets the [slugProvider] to be used globally for slug generation.
      *
-     * @param provider The [ISlugProvider] instance to set.
+     * @param provider The [slugProvider] instance to set.
      * @throws IllegalArgumentException if the provider is null.
      */
     fun setSlugProvider(provider: ISlugProvider?) {
-        ISlugProvider = provider
+        slugProvider = provider
     }
 
     /**
-     * Retrieves the currently set [ISlugProvider].
+     * Retrieves the currently set [slugProvider].
      *
-     * @return The current [ISlugProvider].
-     * @throws SlugOperationException if no [ISlugProvider] has been set.
+     * @return The current [slugProvider].
+     * @throws SlugOperationException if no [slugProvider] has been set.
      */
-    fun getSlugProvider(): ISlugProvider = ISlugProvider ?: throw SlugOperationException("ISlugProvider not set")
+    fun getSlugProvider(): ISlugProvider = slugProvider ?: throw SlugOperationException("ISlugProvider not set")
 }
